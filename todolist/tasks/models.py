25850bc9a92e31f96481451a52923a9c4fe266f4
ci-sumi <tharayilsurendransumi@gmail.com>
2025-02-26 17:26:48 +0000

    Create Task model

--- todolist/tasks/models.py
+++ todolist/tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    complted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+      return self.title
+  
+    
+
 # Create your models here.
