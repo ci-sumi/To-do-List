b7c44aeb41e672449a9fd36a1213a9e7f15fe386
ci-sumi <tharayilsurendransumi@gmail.com>
2025-02-26 18:47:05 +0000

    Add task_list view to retrieve all tasks

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializer import TaskSerializer
 # Create your views here.
+#Retrieve all tasks
+@api_view(['GET'])
+def task_list(request):
+    tasks = Task.objects.all()
+    serieliazer = TaskSerializer(tasks,many=True)
+    return Response(serieliazer.data)
+    
