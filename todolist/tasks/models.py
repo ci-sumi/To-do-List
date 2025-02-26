422cbe40a349aa601a3438e5ad7e61b106196d94
ci-sumi <tharayilsurendransumi@gmail.com>
2025-02-26 17:50:51 +0000

    update field name of Task model

--- todolist/tasks/models.py
+++ todolist/tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
-    complted = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
