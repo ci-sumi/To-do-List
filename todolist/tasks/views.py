7a8a91241eeb7f197320959201d2d9b5a7970ba4
ci-sumi <tharayilsurendransumi@gmail.com>
2025-02-28 10:44:39 +0000

    Add task_list_id ,task_update and task_delete views

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -11,6 +11,15 @@ def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks,many=True)
     return Response(serializer.data)
+@api_view(['GET'])
+def task_list_id(request,pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    seriealizer = TaskSerializer(task)
+    return Response(seriealizer.data)
+    
 
 @api_view(['POST'])
 def  task_create(request):
@@ -19,5 +28,30 @@ def  task_create(request):
         serializer.save()
         return Response(serializer.data,status = status.HTTP_201_CREATED)
     return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def task_update(request,pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = TaskSerializer(task,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def task_delete(request,pk):
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    task.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+    
         
     
